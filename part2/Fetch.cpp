01d323256d85522c027a0e6df77f9195b8c5280e
Winney <tatakornaow@gmail.com>
2024-10-03 21:13:44 +0700

    edit variable

--- part2/Fetch.cpp
+++ part2/Fetch.cpp
@@ -1,6 +1,6 @@
 #include "Simulator.h"
 
 /* Fetch the instruction at the current PC */
-int fetch(Machine *state){
+int fetch(MachineState *state){
     return state->mem[state->pc]; //return the instruction at PC
 }
