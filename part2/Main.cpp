c6d9be11f44bf1bd44e0eed9a4b81198d763a5b1
Winney <tatakornaow@gmail.com>
2024-10-03 21:13:30 +0700

    edit main Pooh part

--- part2/Main.cpp
+++ part2/Main.cpp
@@ -1,5 +1,5 @@
-#include <studio.h>
-#include <stlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "Simulator.h"
 
@@ -25,6 +25,57 @@ int main (int argc, char *argv) {
         if (instruction == 0) { // Assuming '0' represents the halt instruction
             halt();
             break;
+        } 
+
+        int opcode = (instruction >> 22) & 0x7; //22-24
+        int regA = (instruction >> 19) & 0x7; // 19-21
+        int regB = (instruction >> 16) & 0x7; //16-18
+        int destReg, offset;
+
+        switch (opcode) {
+            case 0:  // ADD
+                destReg = instruction & 0x7;  // rd is in last 3 bits
+                state.reg[destReg] = state.reg[regA] + state.reg[regB];
+                break;
+
+            case 1:  // SUB
+                destReg = instruction & 0x7;  // rd is in last 3 bits
+                state.reg[destReg] = state.reg[regA] - state.reg[regB];
+                break;
+
+            case 2:  // LW
+                offset = instruction & 0xFFFF;
+                state.reg[regB] = state.mem[state.reg[regA] + offset];
+                break;
+
+            case 3:  // SW
+                offset = instruction & 0xFFFF;
+                state.mem[state.reg[regA] + offset] = state.reg[regB];
+                break;
+
+            case 4:  // BEQ
+                offset = instruction & 0xFFFF;
+                if (state.reg[regA] == state.reg[regB]) {
+                    state.pc += offset;
+                }
+                break;
+
+            case 5:  // JALR
+                state.reg[regB] = state.pc;
+                state.pc = state.reg[regA];
+                break;
+
+            case 6:  // HALT
+                printf("machine halted\n");
+                printf("total of %d instructions executed\n", state.pc);
+                return 0;
+
+            case 7:  // NOOP
+                break;
+
+            default:
+                printf("error: illegal opcode %d\n", opcode);
+                return 1;
         }
 
         UpdatePC(&state); // Update PC after executing instruction
@@ -37,7 +88,7 @@ int main (int argc, char *argv) {
 /* Initialize machine state: Set PC to 0 and registers to 0 */
 void initMachineState(MachineState *state) {
     state->pc = 0;
-    memset(state->reg, sizeof(state->reg));
+    memset(state->reg,0, sizeof(state->reg));
 }
 
 /* Load machine code into memory from file */
@@ -45,7 +96,7 @@ void loadMemory(MachineState *state, char *filename) {
     File *filePtr = fopen(filename, "r");
     char line[MAXLINELENGTH];
 
-    if(filePtr == null) {
+    if (filePtr == null) {
         printf("error : can't open file %s\n", filename);
         perror("fopen");
         exit(1);
