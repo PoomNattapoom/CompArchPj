6500f10588b045a74f6a1f949382dd354c117187
Winney <tatakornaow@gmail.com>
2024-10-10 22:42:40 +0700

    remove non use file

--- part2/Fetch.cpp
+++
@@ -1,7 +0,0 @@
-#include "Simulator.h"
-
-/* Fetch the instruction at the current PC */
-int fetch(MachineState *state)
-{
-  return state->mem[state->pc]; // return the instruction at PC
-}
--- part2/Halt.cpp
+++
@@ -1,6 +0,0 @@
-#include "Simulator.h"
-#include <stdio.h>
-
-void halt(){
-    printf("Halt instruction encountered. Stopping simulation. \n");
-}
