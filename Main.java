f7d364bca8dd86c06feb0749e0a8f76dd43202d9
PoomNattapoom <rockyaggi@gmail.com>
2024-10-13 00:27:25 +0700

    up

--- Main.java
+++ Main.java
@@ -34,7 +34,7 @@ public class Main {
 
     try {
 
-      String file = "combine.txt";
+      String file = "multiply.txt";
       // Path to the text file
       String filePath = "assembly/" + file;
 
