6e1fad0a8364f51c005871cb12966ede24060a3c
PoomNattapoom <rockyaggi@gmail.com>
2024-10-12 23:02:18 +0700

    non-used file removed

--- Main.java
+++ Main.java
@@ -34,7 +34,7 @@ public class Main {
 
     try {
 
-      String file = "multiply.txt";
+      String file = "test.txt";
       // Path to the text file
       String filePath = "assembly/" + file;
 
